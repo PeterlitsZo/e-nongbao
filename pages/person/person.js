1f09f253d1f529f25fe22f26aa67baa1e1f66c1d
Peterlits Zo <peterlitszo@outlook.com>
2021-02-27 04:09:56 +0800

    feat: (pages/person/*, *) 完善“个人”页面
    更改以使得个人页面完善。现在“个人”页面可以登录，并且拥有两个图标（设置和反馈）。
    
    修改了：
    
        - `pages/person/person.js`:
            增加了 userInfo 数据结构用来显示用户信息和相关逻辑代码。
        - `pages/person/person.wxml`:
            “个人”页面的类 HTML 描述结构，指明了登录块和两个图标（设置和反馈）。
        - `pages/person/person.wxss`:
            “个人”页面的样式表。
    
    新建了：
    
        - `images/*`:
            “个人”页面所需的相关图标。

--- pages/person/person.js
+++ pages/person/person.js
@@ -1,2 +1,30 @@
 // pages/person/person.js
-Page({ })
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  onLoad() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
+  },
+
+  // 通过参数 userInfo 得到用户信息，被 `./person.wxml` 中登录按钮使用。
+  getUserInfo(userInfo) {
+    app.globalData.userInfo = userInfo.detail.userInfo
+    this.setData({
+      userInfo: userInfo.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
