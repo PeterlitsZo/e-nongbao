b64f63faf1be298ea3142d7a3d8c3d973b130d60
Peterlits Zo <peterlitszo@outlook.com>
2021-03-03 01:35:01 +0800

    feat: (pages/index/index.{js, wxml}) 为“首页”页面的“常用应用”添加图标
    在本 commit 之前，首页的“常用应用”板块并没有图标（而是字符串：“[图标：暂定]”）
    ，因此添加图标。
    
    修改了：
    
        - `pages/index/index.js`:
            增加硬编码中应用对象所对应的图标地址。
        - `pages/index/index.wxml`:
            更改“首页”的描述结构以放下图标。

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,27 +18,35 @@ Page({
     applications: [
       {
         title: "信息发布",
+        pic_addr: "/images/application/信息发布.png"
       },
       {
         title: "到村签到",
+        pic_addr: "/images/application/到村签到.png"
       },
       {
         title: "村民缴费",
+        pic_addr: "/images/application/村民缴费.png"
       },
       {
         title: "失物招领",
+        pic_addr: "/images/application/失物招领.png"
       },
       {
         title: "家电维修",
+        pic_addr: "/images/application/家电上门维修.png"
       },
       {
         title: "好物共享",
+        pic_addr: "/images/application/好物共享专区.png"
       },
       {
-        title: "村医问证",
+        title: "村医问诊",
+        pic_addr: "/images/application/村医问诊.png"
       },
       {
         title: "防疫专区",
+        pic_addr: "/images/application/防疫专区.png"
       },
     ]
   }
