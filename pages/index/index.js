92f49cfe7707d689ab114260412f22f6c0756269
Peterlits Zo <peterlitszo@outlook.com>
2021-03-02 12:31:43 +0800

    feat: (pages/index/*) 完善“首页”页面
    添加“首页”页面，使得它能够显示搜索框、最近新闻轮播图、和常用应用。
    
    修改了：
    
        - `pages/index/index.js`:
            首页页面的相关数据硬编码。
        - `pages/index/index.wxml`:
            首页页面的类 HTML 描述结构，展示了搜索框、最近新闻轮播图、和常用应用等。
        - `pages/index/index.wxss`:
            首页页面的样式表。

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,45 @@
 // index.js
-Page({ })
+Page({
+  data: {
+    news: [
+      {
+        title: "中央一号文件发布，农业板块将受益？别忽略了保险！",
+        pic_addr: "/images/news/01.jpg",
+      },
+      {
+        title: "开年这场大降温预示着什么？农业更大的挑战还在后面！",
+        pic_addr: "/images/news/02.jpg",
+      },
+      {
+        title: "2021中国农业竞争新模式：数字农业",
+        pic_addr: "/images/news/03.jpg",
+      }
+    ],
+    applications: [
+      {
+        title: "信息发布",
+      },
+      {
+        title: "到村签到",
+      },
+      {
+        title: "村民缴费",
+      },
+      {
+        title: "失物招领",
+      },
+      {
+        title: "家电维修",
+      },
+      {
+        title: "好物共享",
+      },
+      {
+        title: "村医问证",
+      },
+      {
+        title: "防疫专区",
+      },
+    ]
+  }
+})
