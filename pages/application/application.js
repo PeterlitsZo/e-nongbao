35509eaf71b32bfd64e82f32807cb3ccbc5e2960
Peterlits Zo <peterlitszo@outlook.com>
2021-03-02 17:14:34 +0800

    feat: (pages/application/*, images/application/*) 完善“应用”页面
    添加“应用”页面，使它能够显示所有的应用组和应用图标。
    
    修改了：
    
        - `pages/application/application.js`:
            应用页面的基本逻辑代码，包含了新闻的硬编码。
        - `pages/application/application.wxml`:
            应用页面的类 HTML 描述结构，展示了所有应用组和应用图标。
        - `pages/application/application.wxss`:
            应用页面的样式表。
    
    新建了：
    
        - `images/application/*`:
            应用页面涉及的相关图片。

--- pages/application/application.js
+++ pages/application/application.js
@@ -1,2 +1,101 @@
 // pages/application/application.js
-Page({ })
+Page({
+  data: {
+    applications: {
+      "财务与生活": [
+        {
+          title: "村情村貌",
+          pic_addr: "/images/application/村情村貌.png",
+        },
+        {
+          title: "到村签到",
+          pic_addr: "/images/application/到村签到.png",
+        },
+        {
+          title: "工作汇报",
+          pic_addr: "/images/application/工作汇报.png",
+        },
+        {
+          title: "信息发布",
+          pic_addr: "/images/application/信息发布.png",
+        },
+        {
+          title: "村委评价",
+          pic_addr: "/images/application/村委工作评价.png",
+        },
+        {
+          title: "意见反馈",
+          pic_addr: "/images/application/村民意见反馈.png",
+        },
+        {
+          title: "村民缴费",
+          pic_addr: "/images/application/村民缴费.png",
+        },
+        {
+          title: "失物招领",
+          pic_addr: "/images/application/失物招领.png",
+        },
+        {
+          title: "家电维修",
+          pic_addr: "/images/application/家电上门维修.png",
+        },
+        {
+          title: "好物共享",
+          pic_addr: "/images/application/好物共享专区.png",
+        },
+        {
+          title: "村医问诊",
+          pic_addr: "/images/application/村医问诊.png",
+        },
+        {
+          title: "垃圾处理",
+          pic_addr: "/images/application/垃圾处理.png",
+        },
+        {
+          title: "防疫专区",
+          pic_addr: "/images/application/防疫专区.png",
+        },
+      ],
+      "农产品交易": [
+        {
+          title: "蔬菜豆制品",
+          pic_addr: "/images/application/蔬菜豆制品.png",
+        },
+        {
+          title: "肉禽蛋",
+          pic_addr: "/images/application/肉禽蛋.png",
+        },
+        {
+          title: "水产",
+          pic_addr: "/images/application/水产.png",
+        },
+        {
+          title: "水果",
+          pic_addr: "/images/application/水果.png",
+        },
+        {
+          title: "特惠专区",
+          pic_addr: "/images/application/特惠专区.png",
+        },
+      ],
+      "求职与就业": [
+        {
+          title: "附近",
+          pic_addr: "/images/application/附近.png",
+        },
+        {
+          title: "急招",
+          pic_addr: "/images/application/急招.png",
+        },
+        {
+          title: "企业专区",
+          pic_addr: "/images/application/企业专区.png",
+        },
+        {
+          title: "个人匹配",
+          pic_addr: "/images/application/个人匹配.png",
+        },
+      ]
+    }
+  }
+})
